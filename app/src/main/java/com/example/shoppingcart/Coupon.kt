24983db8510cf18e7f41b82f395df4ac4fc42d33
Neslişah Çelek <neslisah.celek@outlook.com>
2023-04-29 18:15:07 +0300

    Add Product and Coupon Class

---
+++ app/src/main/java/com/example/shoppingcart/Coupon.kt
@@ -0,0 +1,7 @@
+package com.example.shoppingcart
+
+class Coupon(private val couponID: Int,
+             private var discountType: String,
+             private var value:Int ) {
+
+}
---
+++ app/src/main/java/com/example/shoppingcart/Product.kt
@@ -0,0 +1,8 @@
+package com.example.shoppingcart
+
+class Product(private val productID: Int,
+              private var productName: String,
+              private var productPrice:Int,
+              private var categoryID: Int){
+
+}
