f416ae5a4ef92fce25422a24cbb6b0cc0bf2fbaa
Neslişah Çelek <neslisah.celek@outlook.com>
2023-04-30 11:07:20 +0300

    Add view binding

--- app/src/main/java/com/example/shoppingcart/MainActivity.kt
+++ app/src/main/java/com/example/shoppingcart/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.shoppingcart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shoppingcart.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    var shoppingCart = HashMap<Int, Product>()
+    //var shoppingCart = ArrayList<Product>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun addToCart(product: Product) { //add product to shopping cart
+        if(shoppingCart.contains(product.getProductID())) {
+            product.productQuantity++
+        } else {
+            shoppingCart.put(product.getProductID(),product)
+        }
     }
 }
--- app/src/main/java/com/example/shoppingcart/Product.kt
+++ app/src/main/java/com/example/shoppingcart/Product.kt
@@ -3,6 +3,19 @@ package com.example.shoppingcart
 class Product(private val productID: Int,
               private var productName: String,
               private var productPrice:Int,
-              private var categoryID: Int){
+              private var categoryID: Int,
+              var productQuantity: Int = 0) {
+    fun getProductID(): Int {
+        return productID
+    }
+    fun getProductName(): String {
+        return productName
+    }
+    fun getProductPrice(): Int {
+        return productPrice
+    }
+    fun getCategoryID(): Int {
+        return categoryID
+    }
 
 }
