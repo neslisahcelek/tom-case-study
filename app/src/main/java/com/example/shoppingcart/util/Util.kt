df5787cb3c84956eb422d5776d6c0ab221c98439
Neslişah Çelek <neslisah.celek@outlook.com>
2023-05-08 17:27:09 +0300

    Add image download extension

---
+++ app/src/main/java/com/example/shoppingcart/util/Util.kt
@@ -0,0 +1,26 @@
+package com.example.shoppingcart.util
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.shoppingcart.R
+
+fun ImageView.downloadFromUrl(url: String?,progressDrawable: CircularProgressDrawable){
+    val options = RequestOptions()
+        .placeholder(progressDrawable)
+        .error(R.drawable.shopping_cart_48px)
+
+    Glide.with(context)
+        .setDefaultRequestOptions(options)
+        .load(url)
+        .into(this)
+}
+fun placeHolderProgressBar(context:Context):CircularProgressDrawable{
+    return CircularProgressDrawable(context).apply {
+        strokeWidth = 8f
+        centerRadius = 40f
+        start()
+    }
+}
